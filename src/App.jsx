ed7412e998bfe2b9082cf724813cbfb039d490d6
omar sanchez <cosr2000@gmail.com>
2023-07-30 21:01:10 -0600

    proyecto terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
-import { Container, Typography } from "@mui/material"
-
+import { Container, Grid, Typography } from "@mui/material";
+import Formulario from "./components/Formulario";
+import { NoticiasProvider } from "./context/NoticiasProvider";
+import ListadoNoticias from "./components/ListadoNoticias";
 function App() {
-  
-
   return (
-    <Container>
-      <header>
-        <Typography align="center" marginY={5} component="h1" variant="h3">
-          Buscador de Noticias
-        </Typography>
-      </header>
-    </Container>
-  )
+    <NoticiasProvider>
+      <Container>
+        <header>
+          <Typography align="center" marginY={5} component="h1" variant="h3">
+            Buscador de Noticias
+          </Typography>
+        </header>
+
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={12} md={6} lg={4}>
+            <Formulario />
+          </Grid>
+        </Grid>
+
+        <ListadoNoticias/>
+      </Container>
+    </NoticiasProvider>
+  );
 }
 
-export default App
+export default App;
